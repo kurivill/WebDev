d0cb60adf23ea9c16d5b2b6f3e35d52bf0d3a016
kurivill <kurikkaville@hotmail.fi>
2024-10-28 12:45:17 +0200

    5th lab

---
+++ week2/week2-fe-lab5/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+// import './App.css';
+import './styles.css';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+
+
+export default App;
