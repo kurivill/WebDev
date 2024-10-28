3fa08feb4177d2233a0e87dba526d7c74a4b2f7e
kurivill <kurikkaville@hotmail.fi>
2024-10-28 10:29:17 +0200

    second

---
+++ week2/week2-fe-lab3/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+import Bye from './Bye';
+
+
+// Bye.js
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
---
+++ week2/week2-fe-lab3/src/Hello.js
@@ -0,0 +1,7 @@
+// Hello.js
+
+function Hello() {
+    return <p>Hi, React!</p>;
+  }
+  
+  export default Hello;
