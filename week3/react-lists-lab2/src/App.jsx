0fe21cda5421870b54fe650eab234d9e29807318
kurivill <kurikkaville@hotmail.fi>
2024-11-04 09:59:23 +0200

    react-activity 2 first half

---
+++ week3/react-lists-lab2/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BookList from './BookComponent/BookList'
+
+function App() {
+  return <BookList />;
+}
+
+export default App
