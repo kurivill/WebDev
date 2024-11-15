4600b36f0444d4997fd2c388b242e24ac22fb3dd
kurivill <kurikkaville@hotmail.fi>
2024-11-15 11:10:53 +0200

    thought a bit more about improving db.js

--- week4/week4-be-activity2/config/db.js
+++ week4/week4-be-activity2/config/db.js
@@ -14,5 +14,5 @@ module.exports = connectDB;
 
 // Honestly, I don't see many problems with this. Maybe that the connection is hardcoded to localhost, 
 // but I'm not sure if that is a big deal. Maybe the error handling could be improved, since
-// now it just logs it to console and exits.
+// now it just logs it to console and exits. Maybe throwing the error as a json-response would work?
  
